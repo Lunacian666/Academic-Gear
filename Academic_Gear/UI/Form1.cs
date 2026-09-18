307254efac4c36791dd7a09ca5c26b8398db0a8c
Jeremy Kobi Butor <kobibutor03@gmail.com>
2026-09-18 13:51:38 +0800

    Revert "Added log in UI"

--- Academic_Gear/UI/Form1.cs
+++ Academic_Gear/UI/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace UI
 {
-    public partial class LogInAdmin : Form
+    public partial class Form1 : Form
     {
-        public LogInAdmin()
+        public Form1()
         {
             InitializeComponent();
         }
